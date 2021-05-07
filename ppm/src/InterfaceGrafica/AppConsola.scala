b7c32f51e63990c7f92848864030a7e868c5807e
tomas-silva-1 <tomassilva0210@gmail.com>
2021-05-07 14:29:32 +0100

    AppConsola está fixe

--- ppm/src/InterfaceGrafica/AppConsola.scala
+++ ppm/src/InterfaceGrafica/AppConsola.scala
@@ -4,6 +4,7 @@ import package1.BitMap.generateImageFromBitMap
 import package1.{BitMap, Manipulation}
 import package1.Manipulation.{Coords, contrast, generateBitMapFromImage, makeBitMap, makeQTree, mapColourEffect, mirrorH, mirrorV, rotateL, rotateR, scale, sepia}
 import qtrees.QTree
+import random.MyRandom
 
 import scala.io.StdIn.{readDouble, readInt, readLine}
 
@@ -13,7 +14,7 @@ object AppConsola{
 
   def iniciar(): String={
     println("Insira o nome da imagem que deseja alterar:")
-    "C:\\Users\\tomas\\Documents\\Docs\\Iscte\\ppm\\projeto\\Projeto_ppm\\ppm\\src\\imagens\\"+readLine()
+    System.getProperty("user.dir") + "\\ppm\\src\\imagens\\"+readLine()
 
   }
   def loadImage(string: String): QTree[Coords] = {
@@ -33,40 +34,41 @@ object AppConsola{
   }
 
   def chooseFunction(string: String): Unit = {
-    println("1- Scale image")
-    println("2- Mirror image vertically")
-    println("3- Mirror image horizontally")
-    println("4- Rotate image Left")
-    println("5- Rotate image Right")
-    println("6- Apply contrast to the image")
-    println("7- Apply sepia to the image")
-    println("8- Apply noise to the image")
-    println("0- exit")
+    println("1- Dar scale à imagem")
+    println("2- Espelhar a imagem verticalmente")
+    println("3- Espelhar a imagem horizontalmente")
+    println("4- Rodar a imagem para a esquerda")
+    println("5- Rodar a imagem para a direita")
+    println("6- Aplicar contraste à imagem ")
+    println("7- Aplicar sepia à imagem ")
+    println("8- Aplicar noise à imagem ")
+    println("0- salvar e enviar")
     val n = readInt()
     n match {
       case 1 =>{saveImage(scale(getScaleFactor(),loadImage(string)),string)
-        println("Alterações efetuadas á imagem")
+        println("Alterações efetuadas à imagem")
         chooseFunction(string)}
       case 2=>{saveImage(editImage(loadImage(string),mirrorV),string)
-        println("Alterações efetuadas á imagem")
+        println("Alterações efetuadas à imagem")
       chooseFunction(string)}
       case 3=>{saveImage(editImage(loadImage(string),mirrorH),string)
-        println("Alterações efetuadas á imagem")
+        println("Alterações efetuadas à imagem")
         chooseFunction(string)}
       case 4=>{saveImage(editImage(loadImage(string),rotateL),string)
-        println("Alterações efetuadas á imagem")
+        println("Alterações efetuadas à imagem")
         chooseFunction(string)}
       case 5=>{saveImage(editImage(loadImage(string),rotateR),string)
-        println("Alterações efetuadas á imagem")
+        println("Alterações efetuadas à imagem")
         chooseFunction(string)}
       case 6=>{saveImage(mapColourEffect(contrast,loadImage(string)),string)
-        println("Alterações efetuadas á imagem")
+        println("Alterações efetuadas à imagem")
         chooseFunction(string)}
       case 7=>{saveImage(mapColourEffect(sepia,loadImage(string)),string)
-        println("Alterações efetuadas á imagem")
+        println("Alterações efetuadas à imagem")
         chooseFunction(string)}
-    //  case 8=>{saveImage(mapColourEffect(sepia,loadImage(string)),string)
-    //    chooseFunction(string)}
+      case 8=>{val r = MyRandom(3)
+        saveImage(Manipulation(loadImage(string)).mapColourEffectNoise(r),string)
+        chooseFunction(string)}
       case 0=>{}
 
     }
